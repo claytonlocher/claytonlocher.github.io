6079286313bffbc5b1a0fca69bdac201f349ff85
claytonlocher <claytonlocher@gmail.com>
2016-07-25 16:57:22 -0500

    Site redesign

--- js/script.js
+++ js/script.js
@@ -1,19 +1,15 @@
-$(function() {
+document.addEventListener('DOMContentLoaded', function(){
 
-	$('.social-border').hover(function() {
-		$(this).css({
-			'background-color': '#D3D3D3',
-			'border': 'dotted 2px #777777'
-		});
-		$(this).animate({'bottom': '+=3px'}, 200);
-		$(this).find('i').css('color', '#212121');
-	}, function() {
-		$(this).css({
-			'background-color': 'inherit',
-			'border': '2px solid #D3D3D3'
-		});
-		$(this).animate({'bottom': '-=3px'}, 200);
-		$(this).find('i').css('color', 'inherit');
-	});
+  var socialIcons = document.querySelector('.social-icons');
+  var downArrow = document.querySelector('.down-arrow');
+
+  var scrollHandler = function() {
+    socialIcons.style.right = '30px';
+    downArrow.style.display = 'none';
+    window.removeEventListener('scroll', scrollHandler);
+  };
+
+  window.addEventListener('scroll', scrollHandler);
 
 });
+
